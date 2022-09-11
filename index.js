63d6c9a0159dcb28dc919ef61a7c431b7b5fb800
Amogh <amoghjahagirdargsf@gmail.com>
2022-09-11 15:03:21 +0530

    done

--- index.js
+++ index.js
@@ -1,27 +1,96 @@
 console.log("hello world")
 const form = document.querySelector("#submit");
-if (window.ethereum) {
-  console.log('MetaMask is installed!');
-} else {
-    console.log(window)
-}
-const urlParams = new URLSearchParams(location.search)
-const formElements = document.getElementById("submit")
-// const toElem = document.getElementById("toText")
-urlParams.forEach((val,key) => {
-    formElements.elements[key].value =val
-}
-)
-// formElements.elements[key].value = val
-form.addEventListener('submit',(event) => {
-    console.log("clicked submit")
-    event.preventDefault()
-    const formElements = document.getElementById("submit")
-    const to = formElements.elements['to'].value
-    const from = formElements.elements['from'].value
-    const txData = formElements.elements['txData'].value
-    const value = formElements.elements['value'].value
-    const chainId = formElements.elements['chainId'].value
-    console.log("🚀 ~ file: index.js ~ line 8 ~ form.addEventListener ~ to, from,txData, value, chainId", to, from,txData, value, chainId)
 
-})
+const metamaskCheck = checkIfMetamaskExists()
+let currentAccount, currentChainId
+populateFormFromQuery()
+
+// formElements.elements[key].value = val
+// form.addEventListener('submit',(event) => {
+async function sendTransaction(){ 
+console.log("clicked submit")
+    // event.preventDefault()
+    formElements = document.getElementById("submit")
+ 
+    const params = {
+        to : formElements.elements['to'].value,
+        from : formElements.elements['from'].value,
+        txData : formElements.elements['txData'].value,
+        value : formElements.elements['value'].value,
+        chainId : formElements.elements['chainId'].value,
+    }
+     
+    if(!currentAccount){ 
+        await connect()
+    }
+
+    console.log("🚀 ~ file: index.js ~ line 24 ~ sendTransaction ~ currentAccount", currentAccount)
+    if(params.from != currentAccount){
+        console.log("From address is not the same as connected address")
+        return
+    }
+    if("0x" + parseInt(params.chainId,10).toString(16) != currentChainId){
+        console.log("🚀 ~ file: index.js ~ line 33 ~ sendTransaction ~ parseInt(params.chainId,10).toString(16)", parseInt(params.chainId,10).toString(16))
+        console.log("You are connected to the wrong network :", currentChainId)
+        return
+    }
+
+    ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [params],
+    })
+    .then((txHash) => console.log(txHash))
+    .catch((error) => console.error);
+}
+
+ethereum.on('accountsChanged', function (accounts) {
+    currentAccount = accounts[0]
+});
+
+function handleAccountsChanged(){
+    currentAccount = ethereum.selectedAddress
+}
+
+
+async function connect(){
+    if(currentAccount){
+        console.log(`Metamask is already connected with address : ${currentAccount}`)
+        return
+    }
+
+      ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then(handleAccountsChanged)
+    .catch((error) => {
+      if (error.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error("requreAccounts : ",error);
+      }
+    });
+
+}
+
+function populateFormFromQuery(){
+    const urlParams = new URLSearchParams(location.search)
+    let formElements = document.getElementById("submit")
+    urlParams.forEach((val,key) => {
+        formElements.elements[key].value =val
+        }
+    )
+}
+
+function checkIfMetamaskExists(){
+    if (window.ethereum) {
+        return true
+    } else {
+        console.log("Please install Metamask!")
+        return false
+    }
+
+}
+
+ethereum.on('connect', handler => {
+    currentChainId = handler.chainId
+});
